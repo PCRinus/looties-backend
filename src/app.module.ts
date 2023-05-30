42f15942a6e7e7a9c7b5fd7ed939d2d698e7cb94
Mircea Casapu <mircea.casapu@gmail.com>
2023-05-30 20:45:19 +0300

    chore: scaffold profile files
    LOO_27

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,8 +2,9 @@ import { Module } from '@nestjs/common';
 import { ItemModule } from '@item/item.module';
 import { LootboxModule } from './lootbox/lootbox.module';
 import { ChatModule } from './chat/chat.module';
+import { ProfileModule } from './profile/profile.module';
 
 @Module({
-  imports: [ItemModule, LootboxModule, ChatModule],
+  imports: [ItemModule, LootboxModule, ChatModule, ProfileModule],
 })
 export class AppModule {}
