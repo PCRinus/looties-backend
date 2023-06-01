d7093ac38b1031ba37533c68fac3d058286d5120
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-01 03:30:05 +0300

    test: fix tests for service and controller
    LOO-32

--- src/game-responsibly/game-responsibly.controller.spec.ts
+++ src/game-responsibly/game-responsibly.controller.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameResponsiblyController } from './game-responsibly.controller';
+import { GameResponsiblyService } from '@game-responsibly/game-responsibly.service';
+import { AppModule } from 'src/app.module';
 
 describe('GameResponsiblyController', () => {
   let controller: GameResponsiblyController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
       controllers: [GameResponsiblyController],
+      providers: [GameResponsiblyService],
     }).compile();
 
     controller = module.get<GameResponsiblyController>(GameResponsiblyController);
--- src/game-responsibly/game-responsibly.service.spec.ts
+++ src/game-responsibly/game-responsibly.service.spec.ts
@@ -1,11 +1,13 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { GameResponsiblyService } from './game-responsibly.service';
+import { AppModule } from 'src/app.module';
 
 describe('GameResponsiblyService', () => {
   let service: GameResponsiblyService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [AppModule],
       providers: [GameResponsiblyService],
     }).compile();
 
