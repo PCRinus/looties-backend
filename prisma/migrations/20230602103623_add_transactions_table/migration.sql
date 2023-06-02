1082190c0e99f01e88b2d87eb1c64d71732cafbd
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-02 13:36:55 +0300

    feat: add transactions table

---
+++ prisma/migrations/20230602103623_add_transactions_table/migration.sql
@@ -0,0 +1,31 @@
+/*
+  Warnings:
+
+  - You are about to drop the column `winRation` on the `Profile` table. All the data in the column will be lost.
+
+*/
+-- CreateEnum
+CREATE TYPE "TransactionType" AS ENUM ('DEPOSIT', 'WITHDRAWL');
+
+-- CreateEnum
+CREATE TYPE "TransactionStatus" AS ENUM ('PENDING', 'APPROVED', 'DECLINED');
+
+-- AlterTable
+ALTER TABLE "Profile" DROP COLUMN "winRation",
+ADD COLUMN     "winRatio" DOUBLE PRECISION NOT NULL DEFAULT 0;
+
+-- CreateTable
+CREATE TABLE "Transactions" (
+    "id" SERIAL NOT NULL,
+    "type" "TransactionType" NOT NULL,
+    "method" TEXT NOT NULL,
+    "status" "TransactionStatus" DEFAULT 'PENDING',
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "userId" TEXT NOT NULL,
+
+    CONSTRAINT "Transactions_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Transactions" ADD CONSTRAINT "Transactions_userId_fkey" FOREIGN KEY ("userId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
