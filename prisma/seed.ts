d2ca76ad02139c6b0a2e6a9f0010f32ad4241590
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-12 23:18:23 +0300

    fat: add deterministic UUIDs
    LOO-64

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -1,5 +1,6 @@
 import { PrismaClient } from '@prisma/client';
 import { DateTime } from 'luxon';
+import { v5 as uuidv5 } from 'uuid';
 
 const prisma = new PrismaClient();
 
@@ -7,15 +8,20 @@ const seed = async () => {
   const userIds = await seedUsers();
 
   await seedTransactions(userIds);
+  await seedMessages(userIds);
 };
+
+const UUID_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
 
 const seedUsers = async () => {
   await prisma.user.createMany({
     data: [
       {
+        id: uuidv5('user1', UUID_NAMESPACE),
         walletAddress: '0x1234567890123456789012345678901234567890',
       },
       {
+        id: uuidv5('user2', UUID_NAMESPACE),
         walletAddress: '0x1234567890123456789012345678901234567891',
         excludedUntil: DateTime.now().plus({ years: 1 }).toJSDate(),
       },
@@ -79,6 +85,26 @@ const seedTransactions = async (userIds: string[]) => {
         method: 'SOL',
         type: 'WITHDRAWAL',
         status: 'APPROVED',
+      },
+    ],
+  });
+};
+
+const seedMessages = async (userIds: string[]) => {
+  const message1Id = uuidv5('message1', UUID_NAMESPACE);
+  await prisma.message.createMany({
+    data: [
+      {
+        id: message1Id,
+        userId: userIds[0],
+        message: 'Hello World',
+        likedBy: [userIds[1]],
+      },
+      {
+        id: uuidv5('message2', UUID_NAMESPACE),
+        userId: userIds[1],
+        message: 'Hello World Reply',
+        repliedTo: message1Id,
       },
     ],
   });
