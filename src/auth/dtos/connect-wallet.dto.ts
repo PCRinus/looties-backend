aaba31f96574d823b4862bad296de91616bb2fbc
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-12 22:36:19 +0300

    feat: connect to wallet dto
    LOO-53

--- src/auth/dtos/connect-wallet.dto.ts
+++ src/auth/dtos/connect-wallet.dto.ts
@@ -1,1 +1,11 @@
-export class ConnectWalletDto {}
+import { IsNotEmpty, IsString } from 'class-validator';
+
+export class ConnectWalletDto {
+  @IsNotEmpty()
+  @IsString()
+  readonly walletAddress: string;
+
+  @IsNotEmpty()
+  @IsString()
+  readonly signature: string;
+}
