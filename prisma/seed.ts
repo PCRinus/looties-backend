ec71f7fd5bc08061991b8478d75e7f05b539f3e2
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-13 20:29:02 +0300

    chore: seed db with appropriate data
    LOO-34

--- prisma/seed.ts
+++ prisma/seed.ts
@@ -6,9 +6,12 @@ const prisma = new PrismaClient();
 
 const seed = async () => {
   const userIds = await seedUsers();
+  const inventoryIds = await seedInventories(userIds);
 
   await seedTransactions(userIds);
   await seedMessages(userIds);
+  const itemIds = await seedItems(inventoryIds);
+  await seedLiveDrops(itemIds);
 };
 
 const UUID_NAMESPACE = '1b671a64-40d5-491e-99b0-da01ff1f3341';
@@ -35,6 +38,27 @@ const seedUsers = async () => {
   });
 
   return userIds.map((userId) => userId.id);
+};
+
+const seedInventories = async (userIds: string[]) => {
+  await prisma.inventory.createMany({
+    data: [
+      {
+        userId: userIds[0],
+      },
+      {
+        userId: userIds[1],
+      },
+    ],
+  });
+
+  const inventoryIds = await prisma.inventory.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  return inventoryIds.map((inventoryId) => inventoryId.id);
 };
 
 const seedTransactions = async (userIds: string[]) => {
@@ -105,6 +129,73 @@ const seedMessages = async (userIds: string[]) => {
         userId: userIds[1],
         message: 'Hello World Reply',
         repliedTo: message1Id,
+      },
+    ],
+  });
+};
+
+const seedItems = async (inventoryIds: string[]) => {
+  await prisma.item.createMany({
+    data: [
+      {
+        id: uuidv5('item1', UUID_NAMESPACE),
+        name: 'Item 1',
+        dropChance: 0.1,
+        price: 100,
+        type: 'NFT',
+        highestPrice: 100,
+        lowestPrice: 100,
+        inventoryId: inventoryIds[0],
+      },
+      {
+        id: uuidv5('item2', UUID_NAMESPACE),
+        name: 'Item 2',
+        dropChance: 0.5,
+        price: 200,
+        type: 'NFT',
+        highestPrice: 300,
+        lowestPrice: 100,
+        inventoryId: inventoryIds[0],
+      },
+      {
+        id: uuidv5('item3', UUID_NAMESPACE),
+        name: 'Item 3',
+        dropChance: 0.5,
+        price: 150,
+        type: 'NFT',
+        highestPrice: 250,
+        lowestPrice: 100,
+        inventoryId: inventoryIds[1],
+      },
+    ],
+  });
+
+  const itemIds = await prisma.item.findMany({
+    select: {
+      id: true,
+    },
+  });
+
+  return itemIds.map((itemId) => itemId.id);
+};
+
+const seedLiveDrops = async (itemIds: string[]) => {
+  await prisma.liveDrops.createMany({
+    data: [
+      {
+        id: uuidv5('liveDrop1', UUID_NAMESPACE),
+        itemId: itemIds[0],
+        lootboxId: uuidv5('lootbox1', UUID_NAMESPACE),
+      },
+      {
+        id: uuidv5('liveDrop2', UUID_NAMESPACE),
+        itemId: itemIds[1],
+        lootboxId: uuidv5('lootbox1', UUID_NAMESPACE),
+      },
+      {
+        id: uuidv5('liveDrop3', UUID_NAMESPACE),
+        itemId: itemIds[2],
+        lootboxId: uuidv5('lootbox2', UUID_NAMESPACE),
       },
     ],
   });
