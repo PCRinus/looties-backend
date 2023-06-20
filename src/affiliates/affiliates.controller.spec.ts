defa9350ce5718ca77b5967326eb079ef0939f48
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-20 23:10:42 +0300

    tests: fix tests
    LOO-71

--- src/affiliates/affiliates.controller.spec.ts
+++ src/affiliates/affiliates.controller.spec.ts
@@ -1,12 +1,20 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AffiliatesController } from './affiliates.controller';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+import { AffiliatesController } from '@@affiliates/affiliates.controller';
+import { AffiliatesService } from '@@affiliates/affiliates.service';
+import { SharedModule } from '@@shared/shared.module';
+import { UserModule } from '@@user/user.module';
+import { UserService } from '@@user/user.service';
 
 describe('AffiliatesController', () => {
   let controller: AffiliatesController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [UserModule, SharedModule],
       controllers: [AffiliatesController],
+      providers: [UserService, AffiliatesService],
     }).compile();
 
     controller = module.get<AffiliatesController>(AffiliatesController);
--- src/affiliates/affiliates.service.spec.ts
+++ src/affiliates/affiliates.service.spec.ts
@@ -1,13 +1,28 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { AffiliatesService } from './affiliates.service';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+import type { PrismaClient } from '@prisma/client';
+import type { DeepMockProxy } from 'jest-mock-extended';
+import { mockDeep } from 'jest-mock-extended';
+
+import { AffiliatesService } from '@@affiliates/affiliates.service';
+import { PrismaService } from '@@shared/prisma.service';
+import { SharedModule } from '@@shared/shared.module';
 
 describe('AffiliatesService', () => {
   let service: AffiliatesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [SharedModule],
       providers: [AffiliatesService],
-    }).compile();
+    })
+      .overrideProvider(PrismaService)
+      .useValue(
+        mockDeep<PrismaClient>() as unknown as DeepMockProxy<{
+          [K in keyof PrismaClient]: Omit<PrismaClient[K], 'groupBy'>;
+        }>,
+      )
+      .compile();
 
     service = module.get<AffiliatesService>(AffiliatesService);
   });
