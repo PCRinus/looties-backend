77a36bf4da6f5ca948e120ef8f9ae964044b767d
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-21 02:26:01 +0300

    feat: add endpoint to fetch user settings
    LOO-74

--- src/user-settings/user-settings.controller.ts
+++ src/user-settings/user-settings.controller.ts
@@ -1,4 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Param } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 
+import { UserSettingsService } from '@@user-settings/user-settings.service';
+
+type UserSettings = {
+  hideStats: boolean;
+  isAnonymous: boolean;
+  soundEffects: boolean;
+  notifications: boolean;
+};
+
+@ApiTags('User settings')
 @Controller('user-settings')
-export class UserSettingsController {}
+export class UserSettingsController {
+  constructor(private readonly userSettingsService: UserSettingsService) {}
+
+  @Get(':userId')
+  async getUserSettings(@Param('userId') userId: string): Promise<UserSettings> {
+    return this.userSettingsService.getUserSettings(userId);
+  }
+}
--- src/user-settings/user-settings.service.ts
+++ src/user-settings/user-settings.service.ts
@@ -1,4 +1,23 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+import type { UserSettings } from '@prisma/client';
+
+import { PrismaService } from '@@shared/prisma.service';
 
 @Injectable()
-export class UserSettingsService {}
+export class UserSettingsService {
+  constructor(private readonly prisma: PrismaService) {}
+
+  async getUserSettings(userId: string): Promise<UserSettings> {
+    const settings = await this.prisma.userSettings.findUnique({
+      where: {
+        userId,
+      },
+    });
+
+    if (!settings) {
+      throw new NotFoundException('User settings not found');
+    }
+
+    return settings;
+  }
+}
