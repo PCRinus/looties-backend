3911b9e24213a733049aeaa0a2855daed8b6534b
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-22 22:20:27 +0300

    feat: add public decorator for public routes and add authorization to swagger endpoints
    LOO-53

--- src/auth/auth.guard.ts
+++ src/auth/auth.guard.ts
@@ -1,20 +1,31 @@
 import type { CanActivate, ExecutionContext } from '@nestjs/common';
 import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
+import { Reflector } from '@nestjs/core';
 import { JwtService } from '@nestjs/jwt';
 import type { Request } from 'express';
 
 @Injectable()
 export class AuthGuard implements CanActivate {
-  constructor(private jwtService: JwtService, private readonly configService: ConfigService) {}
+  constructor(
+    private jwtService: JwtService,
+    private readonly configService: ConfigService,
+    private readonly reflector: Reflector,
+  ) {}
 
   async canActivate(context: ExecutionContext): Promise<boolean> {
+    const isPublic = this.reflector.get<boolean>('isPublic', context.getHandler());
+
+    if (isPublic) {
+      return true;
+    }
+
     const jwtSecret = this.configService.get<string>('JWT_SECRET');
     const request = context.switchToHttp().getRequest();
     const token = this.extractTokenFromHeader(request);
 
     if (!token) {
-      throw new UnauthorizedException();
+      throw new UnauthorizedException('Invalid JWT token');
     }
 
     const payload = await this.jwtService.verifyAsync(token, { secret: jwtSecret });
