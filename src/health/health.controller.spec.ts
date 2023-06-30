f254cb25fe509db4386bb60695595b81f1a729df
Mircea Casapu <mircea.casapu@gmail.com>
2023-06-30 15:40:14 +0300

    test: fix failing test
    LOO-114

--- src/health/health.controller.spec.ts
+++ src/health/health.controller.spec.ts
@@ -1,12 +1,25 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { HealthController } from './health.controller';
+import { HealthCheckService, TerminusModule } from '@nestjs/terminus';
+import type { TestingModule } from '@nestjs/testing';
+import { Test } from '@nestjs/testing';
+
+import { HealthController } from '@@health/health.controller';
+import { SharedModule } from '@@shared/shared.module';
 
 describe('HealthController', () => {
   let controller: HealthController;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
+      imports: [SharedModule, TerminusModule],
       controllers: [HealthController],
+      providers: [
+        {
+          provide: HealthCheckService,
+          useValue: {
+            get: jest.fn().mockResolvedValue('some result'),
+          },
+        },
+      ],
     }).compile();
 
     controller = module.get<HealthController>(HealthController);
