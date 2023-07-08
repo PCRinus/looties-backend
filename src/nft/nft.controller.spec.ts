f607ca9c1b4a6535d4634f9465f3a5e5e7e8bc78
Mircea Casapu <mircea.casapu@gmail.com>
2023-07-08 17:23:33 +0300

    test: write tests for nft service and controller
    LOO-116

--- src/nft/nft.controller.spec.ts
+++ src/nft/nft.controller.spec.ts
@@ -1,28 +1,63 @@
+import type { DeepMocked } from '@golevelup/ts-jest';
+import { createMock } from '@golevelup/ts-jest';
 import { ConfigModule } from '@nestjs/config';
 import { JwtModule } from '@nestjs/jwt';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import type { Nfts } from '@prisma/client';
+import Decimal from 'decimal.js';
 
 import { NftController } from '@@nft/nft.controller';
 import { NftService } from '@@nft/nft.service';
-import { NftMetadataService } from '@@nft-metadata/nft-metadata.service';
-import { RpcConnectionService } from '@@rpc-connection/rpc-connection.service';
 import { SharedModule } from '@@shared/shared.module';
 
 describe('NftController', () => {
-  let controller: NftController;
+  let nftController: NftController;
+  let nftService: DeepMocked<NftService>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       imports: [SharedModule, JwtModule, ConfigModule],
       controllers: [NftController],
-      providers: [NftService, NftMetadataService, RpcConnectionService],
+      providers: [
+        {
+          provide: NftService,
+          useValue: createMock<NftService>(),
+        },
+      ],
     }).compile();
 
-    controller = module.get<NftController>(NftController);
+    nftController = module.get<NftController>(NftController);
+    nftService = module.get(NftService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  describe('getNftsForUser', () => {
+    it('should return an empty list if user has no deposited NFTs', async () => {
+      const mockedEmptyNftList = [];
+      nftService.getNfts.mockResolvedValue(mockedEmptyNftList);
+      const result = await nftController.getNftsForUser('testUserId');
+
+      expect(result).toBe(mockedEmptyNftList);
+    });
+
+    it('should return a list of NFTs', async () => {
+      const mockedNftList: Nfts[] = [
+        {
+          id: 'mockNftId',
+          mintAddress: 'mockMintAddress',
+          name: 'mockName',
+          symbol: 'mockSymbol',
+          url: 'mockUrl',
+          price: new Decimal(100),
+          userId: 'mockUserId',
+          createdAt: new Date(),
+          updatedAt: new Date(),
+        },
+      ];
+      nftService.getNfts.mockResolvedValue(mockedNftList);
+      const result = await nftController.getNftsForUser('testUserId');
+
+      expect(result).toBe(mockedNftList);
+    });
   });
 });
--- src/nft/nft.service.spec.ts
+++ src/nft/nft.service.spec.ts
@@ -1,25 +1,92 @@
-import { ConfigModule } from '@nestjs/config';
+import type { DeepMocked } from '@golevelup/ts-jest';
+import { createMock } from '@golevelup/ts-jest';
+import { type Nft, PublicKey } from '@metaplex-foundation/js';
+import { InternalServerErrorException } from '@nestjs/common';
 import type { TestingModule } from '@nestjs/testing';
 import { Test } from '@nestjs/testing';
+import type { Nfts } from '@prisma/client';
+import Decimal from 'decimal.js';
 
 import { NftService } from '@@nft/nft.service';
 import { NftMetadataService } from '@@nft-metadata/nft-metadata.service';
-import { RpcConnectionService } from '@@rpc-connection/rpc-connection.service';
-import { SharedModule } from '@@shared/shared.module';
+import { PrismaService } from '@@shared/prisma.service';
 
 describe('NftService', () => {
-  let service: NftService;
+  let nftService: NftService;
+  let nftMetadataService: DeepMocked<NftMetadataService>;
+  let prisma: DeepMocked<PrismaService>;
 
   beforeEach(async () => {
+    jest.clearAllMocks();
+
     const module: TestingModule = await Test.createTestingModule({
-      imports: [SharedModule, ConfigModule],
-      providers: [NftService, NftMetadataService, RpcConnectionService],
+      providers: [
+        NftService,
+        { provide: NftMetadataService, useValue: createMock() },
+        { provide: PrismaService, useValue: createMock() },
+      ],
     }).compile();
 
-    service = module.get<NftService>(NftService);
+    nftService = module.get<NftService>(NftService);
+    nftMetadataService = module.get(NftMetadataService);
+    prisma = module.get(PrismaService);
   });
 
-  it('should be defined', () => {
-    expect(service).toBeDefined();
+  const mockUserId = 'mockUserId';
+
+  const mockedNft: Nfts = {
+    id: 'mockNftId',
+    mintAddress: 'mockMintAddress',
+    name: 'mockName',
+    symbol: 'mockSymbol',
+    url: 'mockUrl',
+    price: new Decimal(100),
+    userId: 'mockUserId',
+    createdAt: new Date(),
+    updatedAt: new Date(),
+  };
+  const mockedNftList: Nfts[] = [mockedNft];
+
+  const mockPublicKeyInput = '5xot9PVkphiX2adznghwrAuxGs2zeWisNSxMW6hU6Hkj';
+  const mockedNftMetadata: Partial<Nft> = {
+    address: new PublicKey(mockPublicKeyInput),
+    name: 'mockedNftName',
+    symbol: 'mockedNftSymbol',
+    uri: 'mockedNftUrl',
+  };
+
+  describe('getNfts', () => {
+    it('should return an empty list of no NFTs are found for a user', async () => {
+      const mockedEmptyNftList = [];
+      prisma.nfts.findMany = jest.fn().mockResolvedValue(mockedEmptyNftList);
+
+      const result = await nftService.getNfts('mockUserId');
+      expect(result).toBe(mockedEmptyNftList);
+    });
+
+    it('should return a list of NFTs for a user', async () => {
+      prisma.nfts.findMany = jest.fn().mockResolvedValue(mockedNftList);
+
+      const result = await nftService.getNfts('mockUserId');
+      expect(result).toBe(mockedNftList);
+    });
+  });
+
+  describe('deposit', () => {
+    it('should throw an Internal Server Error if depositing throws', async () => {
+      prisma.nfts.create = jest.fn().mockRejectedValue(undefined);
+      const result = nftService.deposit(mockUserId, {} as Nft);
+
+      expect(result).rejects.toThrowError(
+        new InternalServerErrorException(`Error while depositing NFT for user ${mockUserId}`),
+      );
+    });
+
+    it('should deposit an NFT', async () => {
+      prisma.nfts.create = jest.fn().mockResolvedValue(mockedNft);
+      const result = await nftService.deposit(mockUserId, mockedNftMetadata as Nft);
+
+      expect(result).toBe(mockedNft);
+    });
   });
 });
