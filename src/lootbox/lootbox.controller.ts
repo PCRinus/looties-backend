02f00be8131d5b781219ec3f62f6e54f1c930983
Mircea Casapu <mircea.casapu@gmail.com>
2023-07-10 16:59:14 +0300

    feat: configure swagger cli to annotate api endpoints
    LOO-137

--- src/lootbox/lootbox.controller.ts
+++ src/lootbox/lootbox.controller.ts
@@ -1,5 +1,5 @@
 import { Body, Controller, Get, Param, Post, Query, UseGuards } from '@nestjs/common';
-import { ApiBearerAuth, ApiTags } from '@nestjs/swagger';
+import { ApiBearerAuth, ApiBody, ApiTags } from '@nestjs/swagger';
 import type { Lootbox } from '@prisma/client';
 import Decimal from 'decimal.js';
 
@@ -10,7 +10,6 @@ import type { AvailableLootboxItems } from '@@lootbox/lootbox.service';
 import { LootboxService } from '@@lootbox/lootbox.service';
 
 @ApiTags('Lootbox')
-@ApiBearerAuth()
 @UseGuards(AuthGuard)
 @Controller('lootbox')
 export class LootboxController {
@@ -22,16 +21,19 @@ export class LootboxController {
     return await this.lootboxService.getAllLootboxes(page);
   }
 
+  @ApiBearerAuth()
   @Get(':userId')
   async getLootboxesForUser(@Param('userId') userId: string, @Query('page') page: number): Promise<Lootbox[]> {
     return await this.lootboxService.getLootboxesForUser(userId, page);
   }
 
+  @ApiBearerAuth()
   @Get(':userId/available-lootbox-items')
   async getAvailableLootboxItems(@Param('userId') userId: string): Promise<AvailableLootboxItems> {
     return await this.lootboxService.getAvailableLootboxItems(userId);
   }
 
+  @ApiBearerAuth()
   @Post(':userId/create-lootbox')
   async createLootbox(@Param('userId') userId: string, @Body() body: CreateLootboxDto): Promise<void> {
     const { name, price, nft, tokens } = body;
